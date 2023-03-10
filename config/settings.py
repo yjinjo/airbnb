f2c5d8920f6aeefeb3294ac66c743506b7adde62
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-10 17:42:28 +0900

    #3. users 앱 생성 및 장고에서 기본으로 제공하는 User 모델을 상속받아 커스터마이징 준비

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # 3rd party apps
     # my apps
     "houses",
+    "users",
 ]
 
 
@@ -129,3 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Auth
+AUTH_USER_MODEL = "users.User"
