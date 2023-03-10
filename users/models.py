ef517894357a02846a47d3847afad5d38eda4989
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-10 17:53:59 +0900

    #3. AbstractUser을 상속받아서 Customizing

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150)
+    is_host = models.BooleanField()
