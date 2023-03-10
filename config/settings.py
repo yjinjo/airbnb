a387ff64a858cf937ebd671a5ef62a0092d13833
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-10 15:55:39 +0900

    #2. houses 앱 생성 및 House 모델 생성

--- config/settings.py
+++ config/settings.py
@@ -33,15 +33,19 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    # django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party apps
+    # my apps
+    "houses",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
