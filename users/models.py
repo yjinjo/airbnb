b7ae5b688bbbd9e3b93efb66eaafb24258f320b1
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-10 18:11:35 +0900

    #3. Defaults

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
