22e05e2b83ed7fcb13271b15091e702c85903b6e
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-13 10:56:26 +0900

    #8. Delete and change URL for version check

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -24,7 +26,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -45,3 +47,6 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
