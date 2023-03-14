bc6fdb937a15d91ec2f2566941443eca4bfff5c2
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-14 13:41:36 +0900

    #9. Transactions: transaction.atomic()

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
+from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -76,22 +76,22 @@ class Rooms(APIView):
                 except Category.DoesNotExist:
                     raise ParseError("Category not found")
 
-                room = serializer.save(
-                    owner=request.user,
-                    category=category,
-                )
+                try:
+                    with transaction.atomic():
+                        room = serializer.save(
+                            owner=request.user,
+                            category=category,
+                        )
 
-                amenities = request.data.get("amenities")
-                for amenity_pk in amenities:
-                    try:
-                        amenity = Amenity.objects.get(pk=amenity_pk)
-                    except Amenity.DoesNotExist:
-                        raise ParseError(f"Amenity with id {amenity_pk} not found.")
+                        amenities = request.data.get("amenities")
+                        for amenity_pk in amenities:
+                            amenity = Amenity.objects.get(pk=amenity_pk)
+                            room.amenities.add(amenity)
 
-                    room.amenities.add(amenity)
-
-                serializer = RoomDetailSerializer(room)
-                return Response(serializer.data)
+                        serializer = RoomDetailSerializer(room)
+                        return Response(serializer.data)
+                except Exception:
+                    raise ParseError("Amenity not found")
             else:
                 return Response(serializer.errors)
         else:
