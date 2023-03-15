367260379be78be488830b79fea09177f8728eee
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-15 15:59:19 +0900

    #10. Create User

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
@@ -11,8 +12,8 @@ class Me(APIView):
 
     def get(self, request):
         user = request.user
-        serializer = serializers.PrivateUserSerializer(user).data
-        return Response(serializer)
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
 
     def put(self, request):
         user = request.user
@@ -23,6 +24,23 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password()
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
