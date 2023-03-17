4fc3a0eeadf0abce1f75b9fe8607f70aa7c58f04
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-17 10:55:39 +0900

    #11. Paginated Relationships

--- rooms/types.py
+++ rooms/types.py
@@ -1,7 +1,10 @@
 import strawberry
+from django.conf import settings
 from strawberry import auto
 from . import models
 from users.types import UserType
+from typing import List
+from reviews.types import ReviewType
 
 
 @strawberry.django.type(models.Room)
@@ -10,3 +13,10 @@ class RoomType:
     name: auto
     kind: auto
     owner: "UserType"
+
+    @strawberry.field
+    def reviews(self, page: int) -> List["ReviewType"]:
+        page_size = settings.PAGE_SIZE
+        start = (page - 1) * page_size
+        end = start + page_size
+        return self.reviews.all()[start:end]
