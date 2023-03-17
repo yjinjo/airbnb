8fa4d6d6456f81071f13ce965ea727b06cdf0076
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-17 12:04:19 +0900

    #11. Mutation GraphQL

--- common/permissions.py
+++ common/permissions.py
@@ -7,5 +7,5 @@ from strawberry.types import Info
 class OnlyLoggedIn(BasePermission):
     message = "You need to be logged in for this!"
 
-    def has_permission(self, source: Any, info: Info):
+    def has_permission(self, source: Any, info: Info, **kwargs):
         return info.context.request.user.is_authenticated
