1d53e9702f549e4c80481852e6ed3e8d90d12e88
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-21 16:17:02 +0900

    #15. Sign Up Modal

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,9 +8,20 @@ import {
 import { Link } from "react-router-dom";
 import { FaAirbnb, FaMoon } from "react-icons/fa";
 import LoginModal from "./LoginModal";
+import SignUpModal from "./SignUpModal";
 
 export default function Header() {
-  const { onOpen, isOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isLoginOpen,
+    onClose: onLoginClose,
+    onOpen: onLoginOpen,
+  } = useDisclosure();
+
+  const {
+    isOpen: isSignUpOpen,
+    onClose: onSignUpClose,
+    onOpen: onSignUpOpen,
+  } = useDisclosure();
 
   return (
     <HStack
@@ -30,10 +41,13 @@ export default function Header() {
           aria-label="Toggle dark mode"
           icon={<FaMoon />}
         />
-        <Button onClick={onOpen}>Log in</Button>
-        <Button colorScheme={"red"}>Sign up</Button>
+        <Button onClick={onLoginOpen}>Log in</Button>
+        <Button onClick={onSignUpOpen} colorScheme={"red"}>
+          Sign up
+        </Button>
       </HStack>
-      <LoginModal isOpen={isOpen} onClose={onClose} />
+      <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+      <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
     </HStack>
   );
 }
