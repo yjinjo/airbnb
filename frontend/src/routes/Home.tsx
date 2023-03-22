867c6e38017618084ccc144bbdbc2822d29e8c59
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-22 14:31:05 +0900

    #16. Photos Grid

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -3,26 +3,10 @@ import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { getRooms } from "../api";
-
-interface IPhoto {
-  pk: string;
-  file: string;
-  description: string;
-}
-
-interface IRoom {
-  pk: number;
-  name: string;
-  country: string;
-  city: string;
-  price: number;
-  rating: number;
-  is_owner: boolean;
-  photos: IPhoto[];
-}
+import { IRoomList } from "../types";
 
 export default function Home() {
-  const { isLoading, data } = useQuery<IRoom[]>(["rooms"], getRooms);
+  const { isLoading, data } = useQuery<IRoomList[]>(["rooms"], getRooms);
 
   return (
     <Grid
