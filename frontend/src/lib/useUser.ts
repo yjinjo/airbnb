7023d8082e5072d4380cb5dd6b8b22a8b027cce6
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-22 16:49:49 +0900

    #17. 로그인한 유저는 아바타를, 로그인하지 않은 유저는 로그인/회원가입 버튼으로

---
+++ frontend/src/lib/useUser.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMe } from "../api";
+
+export default function useUser() {
+  const { isLoading, data, isError } = useQuery(["me"], getMe, {
+    retry: false,
+  });
+
+  return {
+    userLoading: isLoading,
+    user: data,
+    isLoggedIn: !isError,
+  };
+}
