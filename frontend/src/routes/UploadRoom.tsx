2ad4d35e1141d20aafdce88c654b4bcece22a555
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-23 20:24:52 +0900

    #18. Upload Room Initialization

--- frontend/src/routes/UploadRoom.tsx
+++ frontend/src/routes/UploadRoom.tsx
@@ -1,12 +1,24 @@
 import ProtectedPage from "../components/ProtectedPage";
-import HostOnlyPage from "../components/HostOnlyPage";
 import useHostOnlyPage from "../components/HostOnlyPage";
+import { Box, Container, Heading, VStack } from "@chakra-ui/react";
 
 export default function UploadRoom() {
   useHostOnlyPage();
   return (
     <ProtectedPage>
-      <h1>Upload roommmmmmmmmm</h1>
+      <Box
+        pb={40}
+        mt={10}
+        px={{
+          base: 10,
+          lg: 40,
+        }}
+      >
+        <Container>
+          <Heading textAlign={"center"}>Upload Room</Heading>
+          <VStack spacing={5} as="form" mt={5}></VStack>
+        </Container>
+      </Box>
     </ProtectedPage>
   );
 }
